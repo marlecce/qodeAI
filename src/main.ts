fddf270eaaa15dc982694e7f2f342e22f2c320d9
Marco Lecce <marcolecce@unobravo.com>
2024-03-01 22:23:17 +0100

    fix repo url and repo path directory

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import * as fs from "fs";
+
 import { BERTModel } from "./ai/model/BERTModel";
 import { CodeFetcher } from "./code/fetcher/code-fetcher";
 import { GitCodeFetcher } from "./code/fetcher/git/git-code-fetcher";
@@ -8,11 +10,17 @@ import { RedisClient } from "./storage/redis/redis-client";
 import { RedisCodeStorage } from "./storage/redis/redis-code-storage";
 
 async function main() {
-    if (!process.env.REPO_LOCAL_PATH) throw new Error("REPO_PATH env variable is missing: set it in your .env file");
+    if (!process.env.REPO_LOCAL_PATH)
+        throw new Error("REPO_LOCAL_PATH env variable is missing: set it in your .env file");
     if (!process.env.REPO_URL) throw new Error("REPO_URL env variable is missing: set it in your .env file");
 
     const repoPath = process.env.REPO_LOCAL_PATH;
     const repoUrl = process.env.REPO_URL;
+
+    if (fs.existsSync(repoPath)) {
+        fs.rmSync(repoPath, { force: true, recursive: true });
+    }
+    fs.mkdirSync(repoPath);
 
     // Dependency configuration
     const gitCodeFetcher: CodeFetcher = new GitCodeFetcher(repoPath, repoUrl);
